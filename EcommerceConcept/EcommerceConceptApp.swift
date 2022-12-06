90f93052edff218632ce2360c673f8f73e245257
aumetov7 <aumetov7@gmail.com>
2022-12-06 17:11:26 +0600

    Initial Commit

---
+++ EcommerceConcept/EcommerceConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceConceptApp.swift
+//  EcommerceConcept
+//
+//  Created by Акбар Уметов on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
