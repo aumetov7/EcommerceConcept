a349e96105ceb080c3960aa7e43d727cc817bff7
aumetov7 <aumetov7@gmail.com>
2022-12-07 12:16:57 +0600

    Add image extension

---
+++ EcommerceConcept/ImageExtension.swift
@@ -0,0 +1,17 @@
+//
+//  ImageExtension.swift
+//  EcommerceConcept
+//
+//  Created by Акбар Уметов on 6/12/22.
+//
+
+import SwiftUI
+
+extension Image {
+    func resizedToFill(width: CGFloat, height: CGFloat) -> some View {
+        self
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: width, height: height)
+    }
+}
